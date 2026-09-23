2e5dce7f9f3bdb52f0131ccf8e31358820c3580e
Ayelen Juarez <ayejuarez13@gmail.com>
2026-09-22 22:22:23 -0300

    Agregue estructura del juego dados en dados.html . Capture elementos y cree variables en dados.js para el funcionamiento del juego dados

---
+++ js/dados.js
@@ -0,0 +1,11 @@
+// Capturar elementos
+const botonTirar = document.querySelector("#tirar");
+const botonReiniciar = document.querySelector("#reiniciar");
+const puntajesP = document.querySelector("#puntajes");
+const usuarioP = document.querySelector("#usuario");
+const computadoraP = document.querySelector("#computadora");
+
+// Variables de puntaje
+let puntajeUsuario = 0;
+let puntajeComputadora = 0;
+
